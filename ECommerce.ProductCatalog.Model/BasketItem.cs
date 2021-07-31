50b2771782681cbaa59aa108675afe5b2aa9c2fa
Yoav Saroya <yosaroya@microsoft.com>
2021-07-31 08:56:25 +0300

    Add the Actor Model

---
+++ ECommerce.ProductCatalog.Model/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ECommerce.ProductCatalog.Model
+{
+    public class BasketItem
+    {
+        public Guid ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
